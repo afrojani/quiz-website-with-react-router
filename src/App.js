01b9c50b486867e26aa3749604ff85fc5e6d7118
afrojani <sultana.ani2019@gmail.com>
2022-10-12 07:36:03 -0500

    chart added

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: async () => {
+            return fetch('https://openapi.programming-hero.com/api/quiz')
+          },
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const topics = useLoaderData();
+    const individualTopics = topics.data;
+
+    const data = [
+        { id: individualTopics[0].id, total: individualTopics[0].total },
+        { id: individualTopics[1].id, total: individualTopics[1].total },
+        { id: individualTopics[2].id, total: individualTopics[2].total },
+        { id: individualTopics[3].id, total: individualTopics[3].total },
+    ];
+
     return (
         <div>
-            <h1>this is stats</h1>
+            <h4 className='m-5'>This is a Graphical view of total questions in a single topic.</h4>
+            <LineChart width={500} height={400} data={data}>
+                <Line type="monotone" dataKey='total' stroke="#8884d8" />
+                <XAxis dataKey='id' />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </LineChart>
         </div>
     );
 };
