b35298f60bf88c97786dc85ad64e3d84f1a42b7b
afrojani <sultana.ani2019@gmail.com>
2022-10-12 06:04:38 -0500

    event handler and toast added in quiz section

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Card, Form } from 'react-bootstrap';
 
 const Question = ({ questionData }) => {
-    // console.log(questionData);
-    const { question, options } = questionData;
+    console.log(questionData);
+    const { question, options, correctAnswer } = questionData;
     const strQuestion = question.split('<p>');
     const strQuestion2 = strQuestion[1].split('</p>');
+
+
+    const showToastSuccessMessage = () => {
+        toast.success('Success Notification !', {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+
+
+    const showToastErrorMessage = () => {
+        toast.error('Error Notification !', {
+            position: toast.POSITION.TOP_LEFT
+        });
+    };
+
+
+    const selectOption = (e) => {
+        console.log(e.target.innerText);
+        let selectedOption = e.target.innerText;
+        if (selectedOption === correctAnswer) {
+            showToastSuccessMessage()
+        }
+        else { showToastErrorMessage() }
+    }
+
+
+
     return (
         <div>
             <Card style={{ width: '30rem' }} className="mb-2 bg-light p-2">
@@ -17,7 +46,10 @@ const Question = ({ questionData }) => {
                                 <div key={type} className="mb-3 bg-info p-2 rounded-3">
                                     <Form.Check type='radio' id={`check-api-${type}`}>
                                         <Form.Check.Input type={type} isValid />
-                                        <Form.Check.Label className='text-dark fw-bold' >{type}</Form.Check.Label>
+                                        <span>
+                                            <Form.Check.Label onClick={selectOption} className='text-dark fw-bold' >{type}</Form.Check.Label>
+                                            <ToastContainer />
+                                        </span>
                                     </Form.Check>
                                 </div>
                             ))}
